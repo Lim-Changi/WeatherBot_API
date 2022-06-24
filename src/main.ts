7e5d65c5f74443fbd2e662d29ea8dcfb29b9cd3d
changi <limchangi@naver.com>
2022-06-24 15:02:50 +0900

    [Init]: Main Application Class & Swagger Setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class Application {
+  private logger = new Logger(Application.name);
+  private DEV_MODE: boolean;
+
+  constructor(private server: NestExpressApplication) {
+    this.server = server;
+    this.DEV_MODE = process.env.NODE_ENV === 'production' ? false : true;
+  }
+
+  private swagger() {
+    this.server.enableCors({
+      origin: '*',
+      methods: 'GET',
+      optionsSuccessStatus: 200,
+    });
+    const options = new DocumentBuilder()
+      .setTitle('WeatherBot API')
+      .setDescription('API Document')
+      .build();
+
+    const document = SwaggerModule.createDocument(this.server, options);
+    SwaggerModule.setup('docs', this.server, document);
+  }
+
+  async bootstrap() {
+    await this.swagger();
+    await this.server.listen(8080);
+  }
+
+  startLog() {
+    if (this.DEV_MODE) {
+      this.logger.log(`✅ Dev Server on http://localhost:${8080}`);
+    } else {
+      this.logger.log(`✅ Prod Server on port ${8080}`);
+    }
+  }
 }
-bootstrap();
+
+async function bootstrap(): Promise<void> {
+  const server = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const app = new Application(server);
+  await app.bootstrap();
+  app.startLog();
+}
+
+bootstrap().catch((error) => {
+  new Logger('Init').error(error);
+});
