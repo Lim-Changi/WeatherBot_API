ffa88eafeba9cb53f0330fc6bd231838c8b46bba
changi <limchangi@naver.com>
2022-06-24 17:30:39 +0900

    [Init]: Config JOI Validation & Weather API ENV 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
+import { ValidationSchema } from '@app/common/config/validationSchema';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: ValidationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
