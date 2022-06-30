3137de9501723b25e131489a15dcd50fb2f1a476
changi <limchangi@naver.com>
2022-06-30 16:44:15 +0900

    [Feat]: WeatherInfoServiceStub 생성 & Testing Tool Method 추가 &현재 날씨 정보 TestCode 작성

--- __test__/stub/WeatherInfoServiceStub.ts
+++ __test__/stub/WeatherInfoServiceStub.ts
@@ -1,1 +1,73 @@
-export class WeatherInfoServiceStub {}
+import { Weather } from '@app/entity/domain/weather/weather.entity';
+import {
+  ForecastApiResponse,
+  WeatherApiResponse,
+  WeatherCode,
+} from '@app/entity/domain/weather/WeatherInfoApiResponse';
+import { HttpService } from '@nestjs/axios';
+import { WeatherInfoService } from '../../src/Weather/adapter/weatherInfo.service';
+
+export class WeatherInfoServiceStub extends WeatherInfoService {
+  private code: WeatherCode;
+  private temp: number;
+  private rain1h: number;
+  private minTemp: number;
+  private maxTemp: number;
+  private rainPercentage: number;
+
+  constructor(httpService: HttpService) {
+    super(httpService);
+  }
+
+  setTestWeatherInfo(code: WeatherCode, temp: number, rain1h: number): void {
+    this.code = code;
+    this.temp = temp;
+    this.rain1h = rain1h;
+  }
+
+  setTestForecastInfo(
+    code: WeatherCode,
+    minTemp: number,
+    maxTemp: number,
+    rainPercentage: number,
+  ): void {
+    this.code = code;
+    this.minTemp = minTemp;
+    this.maxTemp = maxTemp;
+    this.rainPercentage = rainPercentage;
+  }
+
+  async getCurrentWeather(_weather: Weather): Promise<WeatherApiResponse> {
+    return {
+      timestamp: new Date().getTime(),
+      code: this.code,
+      temp: this.temp,
+      rain1h: this.rain1h,
+    };
+  }
+
+  async getHistoricalWeather(
+    _weather: Weather,
+    _hourOffset: number,
+  ): Promise<WeatherApiResponse> {
+    return {
+      timestamp: new Date().getTime(),
+      code: this.code,
+      temp: this.temp,
+      rain1h: this.rain1h,
+    };
+  }
+
+  async getForecastWeather(
+    _weather: Weather,
+    _hourOffset: number,
+  ): Promise<ForecastApiResponse> {
+    return {
+      timestamp: new Date().getTime(),
+      code: this.code,
+      min_temp: this.minTemp,
+      max_temp: this.maxTemp,
+      rain: this.rainPercentage,
+    };
+  }
+}
--- __test__/unit/service/weather/WeatherService.spec.ts
+++ __test__/unit/service/weather/WeatherService.spec.ts
@@ -1,6 +1,105 @@
+import { Weather } from '@app/entity/domain/weather/weather.entity';
+import { GreetingType } from '@app/entity/domain/weather/WeatherMessageType';
+import { HttpService } from '@nestjs/axios';
+import { WeatherInfoServiceStub } from '@test/stub/WeatherInfoServiceStub';
+import {
+  generateRandomLatLon,
+  generateRandomRain,
+  generateRandomTemp,
+  generateRandomWeatherCode,
+} from '@test/tools/ci-tools';
+import { WeatherService } from '../../../../src/Weather/weather.service';
+import { WeatherCode } from '@app/entity/domain/weather/WeatherInfoApiResponse';
 describe('Weather Service', () => {
-  describe('Greeting Message', () => {
-    it.todo('날씨 서비스 기능 테스트');
+  const httpService = new HttpService();
+  const testWeatherInfoService = new WeatherInfoServiceStub(httpService);
+  const testWeatherService = new WeatherService(testWeatherInfoService);
+  let testWeather: Weather;
+  let testWeatherCode: WeatherCode;
+  let testTemperature: number;
+  let testRain: number;
+
+  beforeEach(() => {
+    const { lat, lon } = generateRandomLatLon();
+    testWeather = Weather.location(lat, lon);
+    testWeatherCode = generateRandomWeatherCode();
+    testTemperature = generateRandomTemp();
+    testRain = generateRandomRain();
+  });
+
+  describe('현재 날씨 정보, Greeting Message', () => {
+    it('현재 날씨가 눈, 강수량이 100mm 이상 => 폭설이 내리고 있어요', async () => {
+      testWeatherInfoService.setTestWeatherInfo(
+        WeatherCode.snow,
+        testTemperature,
+        150,
+      );
+      await testWeatherService.setWeatherMessage(testWeather);
+      expect(testWeather.summary().greeting).toBe(GreetingType[0]);
+    });
+    it('현재 날씨가 눈, 강수량이 100mm 미만 => 눈이 포슬포슬 내립니다.', async () => {
+      testWeatherInfoService.setTestWeatherInfo(
+        WeatherCode.snow,
+        testTemperature,
+        50,
+      );
+      await testWeatherService.setWeatherMessage(testWeather);
+      expect(testWeather.summary().greeting).toBe(GreetingType[1]);
+    });
+    it('현재 날씨가 비, 강수량이 100mm 이상 => 폭우가 내리고 있어요.', async () => {
+      testWeatherInfoService.setTestWeatherInfo(
+        WeatherCode.rain,
+        testTemperature,
+        150,
+      );
+      await testWeatherService.setWeatherMessage(testWeather);
+      expect(testWeather.summary().greeting).toBe(GreetingType[2]);
+    });
+    it('현재 날씨가 비, 강수량이 100mm 미만 => 비가 오고 있습니다.', async () => {
+      testWeatherInfoService.setTestWeatherInfo(
+        WeatherCode.rain,
+        testTemperature,
+        50,
+      );
+      await testWeatherService.setWeatherMessage(testWeather);
+      expect(testWeather.summary().greeting).toBe(GreetingType[3]);
+    });
+    it('현재 날씨가 흐림 => 날씨가 약간은 칙칙해요.', async () => {
+      testWeatherInfoService.setTestWeatherInfo(
+        WeatherCode.cloudy,
+        testTemperature,
+        testRain,
+      );
+      await testWeatherService.setWeatherMessage(testWeather);
+      expect(testWeather.summary().greeting).toBe(GreetingType[4]);
+    });
+    it('현재 날씨가 맑음, 현재 온도 30도 이상 => 따사로운 햇살을 맞으세요.', async () => {
+      testWeatherInfoService.setTestWeatherInfo(
+        WeatherCode.sunny,
+        50,
+        testRain,
+      );
+      await testWeatherService.setWeatherMessage(testWeather);
+      expect(testWeather.summary().greeting).toBe(GreetingType[5]);
+    });
+    it('현재 날씨가 맑음, 현재 온도 0도 이하 => 날이 참 춥네요.', async () => {
+      testWeatherInfoService.setTestWeatherInfo(
+        WeatherCode.sunny,
+        -50,
+        testRain,
+      );
+      await testWeatherService.setWeatherMessage(testWeather);
+      expect(testWeather.summary().greeting).toBe(GreetingType[6]);
+    });
+    it('현재 날씨가 맑음, 0 < 현재 온도 < 30  => 날씨가 참 맑습니다.', async () => {
+      testWeatherInfoService.setTestWeatherInfo(
+        WeatherCode.sunny,
+        15,
+        testRain,
+      );
+      await testWeatherService.setWeatherMessage(testWeather);
+      expect(testWeather.summary().greeting).toBe(GreetingType[7]);
+    });
   });
 
   describe('Temperature Message', () => {
